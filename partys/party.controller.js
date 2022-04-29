3f147e21a4bd3ac33056d7f23054cd5fba799e0f
William Bogie <william.c.bogie-1@ou.edu>
2022-04-29 11:29:54 -0500

    added controller for party

---
+++ partys/party.controller.js
@@ -0,0 +1,44 @@
+//Handle routes for the User
+const express = require('express');
+const router = express.Router();
+const partyService = require('./party.service');
+
+// routes
+router.post('/register', register);
+router.get('/', getAll);
+router.get('/:id', getById);
+router.put('/:id', update);
+router.delete('/:id', _delete);
+
+module.exports = router;
+
+//Fix
+function register(req, res, next) {
+    userService.create(req.body)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+function getAll(req, res, next) {
+    partyService.getAll()
+        .then(partys => res.json(partys))
+        .catch(err => next(err));
+}
+
+function getById(req, res, next) {
+    partyService.getById(req.params.id)
+        .then(party => party ? res.json(party) : res.sendStatus(404))
+        .catch(err => next(err));
+}
+
+function update(req, res, next) {
+    partyService.update(req.params.id, req.body)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+function _delete(req, res, next) {
+    userService.delete(req.params.id)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
